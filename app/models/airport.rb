4d49318058c4b878322f5970d055b3aa92369254
rdalin82 <Robert.Dalin@gmail.com>
2016-05-24 21:22:09 -0500

    added airport tests for model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,6 @@
 class Airport < ActiveRecord::Base
-  validates :code, uniqueness: true
+  validates :code, uniqueness: true, presence: true
+  validates :name, presence: true
   has_many :departing_flights, class_name: "Flight", foreign_key: "to_airport_id"
   has_many :arriving_flights, class_name: "Flight", foreign_key: "from_airport_id"
 end
--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -4,4 +4,31 @@ class AirportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup 
+    @emptyAirport = Airport.new
+    @validAirport = Airport.new(code: "ABC", name: "ABC Airport")
+    @onlyname = Airport.new(name: "2nd airport")
+    @onlycode = Airport.new(code: "XYZ")
+  end
+
+  def teardown
+    @emptyAirport = nil
+    @validAirport = nil
+  end
+
+  test "Should reject empty airport" do 
+    assert_not @emptyAirport.save
+  end
+
+  test "Should reject airport with only name" do 
+    assert_not @onlyname.save
+  end
+
+  test "Should reject airport with only code" do 
+    assert_not @onlycode.save
+  end
+  
+  test "Should save a valid airport" do 
+    assert @validAirport.save
+  end
 end
