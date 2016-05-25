1d9400f300630da4baa1f5b0e621b3b9f40296d4
rdalin82 <Robert.Dalin@gmail.com>
2016-05-24 21:35:30 -0500

    added flight tests

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,10 @@ class Flight < ActiveRecord::Base
   belongs_to :from_airport, class_name: Airport
   belongs_to :to_airport, class_name: Airport
   validate :valid_flight
+  validates :from_airport, presence: true
+  validates :to_airport, presence: true
+  validates :duration, presence: true
+  validates :start, presence: true 
 
   def valid_flight
     if self.from_airport == self.to_airport
--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @from = Airport.create!(code: "ABC", name: "ABC, USA")
+    @to = Airport.create!(code: "XYZ", name: "XYZ, USA")
+    @validFlight = Flight.new(start: Date.today, from_airport: @from, to_airport: @to, duration: 200)
+    @novalidFlight = Flight.new(from_airport: @from, to_airport: @to)
+  end
+
+  test "Should save flight" do 
+    assert @validFlight.save
+  end
+
+  test "Should not save invalid flight" do 
+    assert_not @novalidFlight.save
+  end
 end
