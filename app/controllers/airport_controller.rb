96ed4d12262c4726827e4cca0816209b57c42cd8
rdalin82 <Robert.Dalin@gmail.com>
2016-06-12 06:29:26 -0500

    changed airport controller to pass html test

--- app/controllers/airport_controller.rb
+++ app/controllers/airport_controller.rb
@@ -2,6 +2,7 @@ class AirportController < ApplicationController
   def index
     @airports = Airport.all
     respond_to do |format|
+      format.html
       format.json { render json: @airports }
     end
   end
